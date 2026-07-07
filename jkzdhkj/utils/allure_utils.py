1bb8c68380e672ce3356c2ea8c3d78c0b06d8a6c
Liyg <154428608+liyang123253@users.noreply.github.com>
2026-07-07 09:18:46 +0800

    Delete jkzdhkj directory

--- jkzdhkj/utils/allure_utils.py
+++
@@ -1,9 +0,0 @@
-import allure
-
-
-def allure_init(case):
-
-    allure.dynamic.feature(case["feature"])
-    allure.dynamic.story(case["story"])
-    # allure.dynamic.title(case["title"])
-    allure.dynamic.title(f"ID:{case["id"]}--{case["title"]}")
